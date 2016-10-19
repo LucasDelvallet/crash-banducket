98b9d432ab53fd57e47e61e9eab7b56d800a25bc
LucasDelvallet <lucas.delvallet@hotmail.fr>
2016-10-19 11:33:01 +0200

    Added possibility to check all new stack trace.

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,5 +1,7 @@
 package main;
 
+import java.io.File;
+import java.io.FilenameFilter;
 import java.io.IOException;
 import java.util.List;
 
@@ -13,15 +15,28 @@ import process.StoreStack;
 public class Main {
 
     public static void main(String[] args) {
-	List<Bucket> buckets = BucketCreator.GetAllBucketInFolder("./nautilus/nautilus-training/");
-	StoreStack storeStack = new StoreStack(buckets);
-	try {
-	    Stack stack = StackCreator.getStackFromFilePath("./nautilus/nautilus-testing/10.txt"); // res = 1337
-	    Bucket b = storeStack.getBestBucket(stack);
-	} catch (IOException e) {
-	    e.printStackTrace();
+		List<Bucket> buckets = BucketCreator.GetAllBucketInFolder("./nautilus/nautilus-training/");
+		StoreStack storeStack = new StoreStack(buckets);
+		
+		try {
+			File folder = new File("./nautilus/nautilus-testing/");
+			String[] files = folder.list(new FilenameFilter() {
+			    @Override
+			    public boolean accept(File current, String name) {
+				return new File(current, name).isFile();
+			    }
+			});
+			
+			for (String file : files) {
+				Stack stack = StackCreator.getStackFromFilePath("./nautilus/nautilus-testing/" + file); // res = 1337
+			    Bucket b = storeStack.getBestBucket(stack);
+			    
+				System.out.println(file.substring(0, file.length()-4) + "  -> " + b.getId());
+			}
+			
+		    
+		} catch (IOException e) {
+		    e.printStackTrace();
+		}
 	}
-	
-    }
-    
 }
--- src/process/StoreStack.java
+++ src/process/StoreStack.java
@@ -15,60 +15,64 @@ public class StoreStack {
     }
     
     public Bucket getBestBucket(Stack stack) {
-	int position = 0;
-	double bestValue = 0;
-	
-	for(int c=0; c < buckets.size(); c++) {
-	    double value = getBucketComparisonValue(stack, buckets.get(c));
+		int position = 0;
+		double bestValue = 0;
+		
+		for(int c=0; c < buckets.size(); c++) {
+		    double value = getBucketComparisonValue(stack, buckets.get(c));
+		    
+		    //System.out.println("Bucket : " + buckets.get(c).getId() + " Valeur : " + value + "%" );
+		    
+		    if(value > bestValue) {
+		    	bestValue = value;
+		    	position = c;
+		    }
+		}
+		
+	    //System.out.println("Bucket choisi : " + buckets.get(position).getId() + " Valeur : " + bestValue + "%" );
 	    
-	    if(value > bestValue) {
-		bestValue = value;
-		position = c;
-	    }
-	}
-	
-	return buckets.get(position);
+		return buckets.get(position);
     }
     
     private double getBucketComparisonValue(Stack stack, Bucket bucket) {
-	ArrayList<Double> values = new ArrayList<>();
-	List<Stack> stacks = bucket.getStacks();
-	
-	for(Stack stackTest : stacks) {
-	    values.add(getStackComparisonValue(stack, stackTest));
-	}
-	
-	return getSumValues(values)/values.size();
+		ArrayList<Double> values = new ArrayList<>();
+		List<Stack> stacks = bucket.getStacks();
+		
+		for(Stack stackTest : stacks) {
+		    values.add(getStackComparisonValue(stack, stackTest));
+		}
+		
+		return getSumValues(values)/values.size();
     }
     
     private double getStackComparisonValue(Stack stackTest, Stack stack) {
-	ArrayList<Double> values = new ArrayList<>();
-	List<StackElement> stackElements = stack.getElements();
-	List<StackElement> stackElementTests = stackTest.getElements();
-	
-	for(StackElement element : stackElements) {
-	    double bestValue = 0;
-	    
-	    for(StackElement elementTest : stackElementTests) {
-		double currentValue = StringSimilarity.similarity(element.getSource(), elementTest.getSource());
-		if(currentValue > bestValue) {
-		    bestValue = currentValue;
+		ArrayList<Double> values = new ArrayList<>();
+		List<StackElement> stackElements = stack.getElements();
+		List<StackElement> stackElementTests = stackTest.getElements();
+		
+		for(StackElement element : stackElements) {
+		    double bestValue = 0;
+		    
+		    for(StackElement elementTest : stackElementTests) {
+				double currentValue = StringSimilarity.similarity(element.getSource(), elementTest.getSource());
+				if(currentValue > bestValue) {
+				    bestValue = currentValue;
+				}
+		    }
+		    
+		    values.add(bestValue);
 		}
-	    }
-	    
-	    values.add(bestValue);
-	}
-	
-	return getSumValues(values)/values.size();
+		
+		return getSumValues(values)/values.size();
     }
     
     private double getSumValues(List<Double> values) {
-	double res = 0;
-	
-	for(Double value : values) {
-	    res += value;
-	}
-	
-	return res;
+		double res = 0;
+		
+		for(Double value : values) {
+		    res += value;
+		}
+		
+		return res;
     }
 }
